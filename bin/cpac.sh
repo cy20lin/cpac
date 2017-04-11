3ce97cb15106bc6bf90f186a30ec514232499c6d
ChienYu Lin <cy20lin@gmail.com>
2017-04-11 15:07:57 +0800

    Add features for customization, downloading ...
    * New features
    
    - load custom init file ('~/.cpac.sh', '~/.cpac.d/init.sh') on startup
    - use flag '--download' to download a package
    - use flag '--repo' to get list of repos to packages

--- bin/cpac.sh
+++ bin/cpac.sh
@@ -6,29 +6,34 @@
 ## License: MIT
 ##
 
-dos2unix(){
+cpac_message () {
+    echo "-- [cpac] " "$@" 1>&2
+}
+
+cpac_dos2unix () {
     sed 's@\r\n@\n@g'
 }
 
-load_package_metadata () {
-    metadata=$(cat "${default_package_metadata}" \
-    | dos2unix \
+cpac_load_package_metadata () {
+    # note: '\x27' => single quote (')
+    cpac_metadata=$(cat "$1" \
+    | cpac_dos2unix \
     | sed '
     /^#.*/ d; # remove comments
     /^.*:$/ s/[^A-Za-z0-9:]//g; # remove special characters
     /^  [^:]*:.*$/ s/: /=/g; # remove special characters
     ' \
     | tr '\n' ';' \
-    | sed 's/;  /#!#/g' \
+    | sed 's/;  /#!#cpac_/g' \
     | tr ';' '\n' \
-    | sed 's/:#!#/="/g; s/#!#/;/g; s/$/"/g' \
+    | sed 's/:#!#/=\x27/g; s/#!#/;/g; s/$/\x27/g' \
     | awk '
     BEGIN {
         FS="=";
         OFS="=";
         i = 0;
         packages="";
-    } 
+    }
     {
         if (/^[A-Za-z][A-Za-z0-9]*=/) {
         package = tolower($1);
@@ -40,86 +45,89 @@ load_package_metadata () {
         else {
             packages = packages ":" $1;
         }
-        print "PACKAGE_" $0;
+            print "CPAC_PACKAGE_" $0;
         }
         else {
         }
     }
     END {
-        print "PACKAGES=" packages;
+        print "CPAC_PACKAGES=\x27" packages "\x27";
     }
    ')
-    eval "${metadata}"
+    eval "${cpac_metadata}"
 }
 
-normalize_name () {
-  echo "${name}" | sed 's/[^a-zA-Z0-9]//g' | awk '{ print tolower($0); }'
+cpac_load_default_package_metadata () {
+    cpac_load_package_metadata "${CPAC_DEFAULT_PACKAGE_METADATA}"
 }
 
-configure_build_install () {
-    test -e "${binary_dir}" && rm -rf -- "${binary_dir}" 
-    mkdir -p -- "${binary_dir}"
-    pushd "${binary_dir}" >/dev/null
-    echo "-- [cpac] configuring..." 1>&2
-    cmake "${@}" "${source_dir}" || ( popd >/dev/null && return 1 )
-    echo "-- [cpac] building..." 1>&2
-    cmake --build . || ( popd >/dev/null && return 1 )
-    echo "-- [cpac] testing..." 1>&2
-    ctest || ( popd >/dev/null && return 1 )
-    echo "-- [cpac] installing..." 1>&2
-    cmake --build . --target install || ( popd >/dev/null && return 1 )
+cpac_normalize_name () {
+  echo "${cpac_name}" | sed 's/[^a-zA-Z0-9]//g' | awk '{ print tolower($0); }'
+}
+
+cpac_configure_build_install () {
+    test -e "${cpac_binary_dir}" && rm -rf -- "${cpac_binary_dir}"
+    mkdir -p -- "${cpac_binary_dir}"
+    pushd "${cpac_binary_dir}" >/dev/null
+    cpac_message "configuring..."
+    cmake ${CPAC_CUSTOM_CONFIGURE_ARGS} "${@}" "${cpac_source_dir}" || ( popd >/dev/null && return 1 )
+    cpac_message "building..."
+    cmake --build . ${CPAC_CUSTOM_BUILD_ARGS} || ( popd >/dev/null && return 1 )
+    cpac_message "testing..."
+    ctest ${CPAC_CUSTOM_TEST_ARGS} || ( popd >/dev/null && return 1 )
+    cpac_message "installing..."
+    cmake --build . --target install ${CPAC_CUSTOM_INSTALL_ARGS}  || ( popd >/dev/null && return 1 )
     popd >/dev/null
     return 0
 }
 
-clear_binary_dir() {
-    echo "-- [cpac] clearing binary_dir..." 1>&2
-    test -e "${binary_dir}" && rm -rf -- "${binary_dir}"
+cpac_clear_binary_dir () {
+    cpac_message "clearing binary_dir..."
+    test -e "${cpac_binary_dir}" && rm -rf -- "${cpac_binary_dir}"
 
 }
 
-retrive_source () {
-    echo "-- [cpac] retriving source..." 1>&2
-    git clone "${repo}" "${source_dir}" || return 1
-    pushd "${source_dir}" >/dev/null 
-    case "_${branch}" in
+cpac_retrive_source () {
+    cpac_message "retriving source..."
+    git clone "${cpac_repo}" "${cpac_source_dir}" || return 1
+    pushd "${cpac_source_dir}" >/dev/null
+    case "_${cpac_branch}" in
     _) popd >/dev/null && return 0 ;;
     _@)
-        branch="tags/$(git describe --abbrev=0 --tags)"
-        if test "${branch}" != "tags/" 
+        cpac_branch="tags/$(git describe --abbrev=0 --tags)"
+        if test "${cpac_branch}" != "tags/"
         then
-            echo "-- [cpac] git checkout '${branch}'" 1>&2
-            git checkout "${branch}" || ( popd >/dev/null && return 1 )
+            cpac_message "git checkout '${cpac_branch}'"
+            git checkout "${cpac_branch}" || ( popd >/dev/null && return 1 )
         fi
         ;;
     _@*)
-        branch=$(echo "${branch}" | sed 's/^.//g')
-        echo "-- [cpac] git checkout '${branch}'" 1>&2
-        git checkout "${branch}" || ( popd >/dev/null && return 1 )
+        cpac_branch=$(echo "${cpac_branch}" | sed 's/^.//g')
+        cpac_message "git checkout '${cpac_branch}'"
+        git checkout "${cpac_branch}" || ( popd >/dev/null && return 1 )
         ;;
     esac
     popd >/dev/null
-    echo done
     return 0
 }
 
-clear_source_dir () {
-    echo "-- [cpac] clearing source_dir..." 1>&2
-    test -e "${source_dir}" && rm -rf -- "${source_dir}"
+cpac_clear_source_dir () {
+    cpac_message "clearing source_dir..."
+    test -e "${cpac_source_dir}" && rm -rf -- "${cpac_source_dir}"
 }
 
-clear_current_package_variables() {
+cpac_clear_current_package_variables() {
     ARGC=
-    name=
-    normalized_name=
-    repo=
-    source_dir=
-    binary_dir=
-    branch=
+    cpac_name=
+    cpac_normalized_name=
+    cpac_repo=
+    cpac_source_dir=
+    cpac_binary_dir=
+    cpac_branch=
 }
 
-setup_current_package_variables () {
-    clear_current_package_variables
+cpac_setup_current_package_variables () {
+    cpac_clear_current_package_variables
     eval $(echo "$1" \
         | sed '/^.*@/ s/^\(.*\)@.*$/\1/g; t end; s/.*/\0/g; :end' \
         | awk '
@@ -142,66 +150,57 @@ setup_current_package_variables () {
         eval $(echo ${ARGV0} | awk '
           {
             if (/^[a-zA-Z][a-zA-Z0-9_.+\-]*$/) {
-                print "spec_type=name;name=\"" $0 "\""
+                print "cpac_spec_type=name;cpac_name=\"" $0 "\""
             } else {
-                print "spec_type=repo;repo=\"" $0 "\""
+                print "cpac_spec_type=repo;cpac_repo=\"" $0 "\""
             }
           }
         ')
-        # echo spec_type=$spec_type
-        if test "${spec_type}" == "repo"
+        if test "${cpac_spec_type}" == "repo"
         then
-            name=$(echo "${ARGV0}" | sed 's@^.*/@@g')
-            normalized_name="$(normalize_name)"
+            cpac_name=$(echo "${ARGV0}" | sed 's@^.*/@@g')
+            cpac_normalized_name="$(cpac_normalize_name)"
         else
-            normalized_name="$(normalize_name)"
-            ref="PACKAGE_${normalized_name}"
-            if eval test "\${${ref}-NOT_FOUND}" == NOT_FOUND
+            cpac_normalized_name="$(cpac_normalize_name)"
+            cpac_ref="CPAC_PACKAGE_${cpac_normalized_name}"
+            if eval test "\${${cpac_ref}-NOT_FOUND}" == NOT_FOUND
             then
-                echo "-- [cpac] package '${name}' metadata not found." 1>&2
-                clear_current_package_variables
+                cpac_message "package '${cpac_name}' metadata not found."
+                cpac_clear_current_package_variables
                 return 1
             else
-                eval "config=\${${ref}}"
-                eval "${config}"
+                eval "cpac_config=\${${cpac_ref}}"
+                eval "${cpac_config}"
             fi
         fi
         ;;
     2)
-        name="$ARGV0"
-        repo="$ARGV1"
-        normalized_name="$(normalize_name)"
-        echo name=$name
-        echo repo=$repo
+        cpac_name="$ARGV0"
+        cpac_repo="$ARGV1"
+        cpac_normalized_name="$(cpac_normalize_name)"
         ;;
     *)
-        clear_current_package_variables
+        cpac_clear_current_package_variables
         return $(false)
         ;;
     esac
-    source_dir="${build_dir}/Source/${name}"
-    binary_dir="${build_dir}/Build/${name}"
-    branch=$(echo "$1" | sed '/^.*@/ s/^.*\(@.*\)$/\1/g; t end; s/.*//g; :end')
+    cpac_source_dir="${CPAC_BUILD_DIR}/Source/${cpac_name}"
+    cpac_binary_dir="${CPAC_BUILD_DIR}/Build/${cpac_name}"
+    cpac_branch=$(echo "$1" | sed '/^.*@/ s/^.*\(@.*\)$/\1/g; t end; s/.*//g; :end')
 }
 
-show_current_package_variables () {
-    echo "-- [cpac] package: $name" 1>&2
-    echo "-- [cpac]   name: $name" 1>&2
-    echo "-- [cpac]   normalized_name: $normalized_name" 1>&2
-    echo "-- [cpac]   repo: $repo" 1>&2
-    echo "-- [cpac]   branch: $branch" 1>&2
-    echo "-- [cpac]   source_dir: $source_dir" 1>&2
-    echo "-- [cpac]   binary_dir: $binary_dir" 1>&2
+cpac_show_current_package_variables () {
+    cpac_message "package: $cpac_name"
+    cpac_message "  name: $cpac_name"
+    cpac_message "  normalized_name: $cpac_normalized_name"
+    cpac_message "  repo: $cpac_repo"
+    cpac_message "  branch: $cpac_branch"
+    cpac_message "  source_dir: $cpac_source_dir"
+    cpac_message "  binary_dir: $cpac_binary_dir"
 }
 
-setup_common_variables () {
-    this_dir="$(dirname $(readlink -f $0))"
-    default_package_metadata="${this_dir}/../etc/cpac.d/packages.yml"
-    build_dir="${this_dir}/../var/cache/cpac"
-}
-
-do_install () {
-    format=$(echo $# | awk '
+cpac_sync () {
+    cpac_format=$(echo $# | awk '
     {
         if ($0 >= 1) {
           output = "%s";
@@ -211,14 +210,14 @@ do_install () {
         }
         print output;
     }')
-    package_spec_list=$(printf "${format}\n" "$@" | awk '
+    cpac_package_spec_list=$(printf "${cpac_format}\n" "$@" | awk '
     BEGIN { stop = 0; }
     {
       if (/^--/) { stop = 1; }
       if (/^$/) { next; }
       if (!stop) { print $0; }
     }')
-    shift_count=$(printf "${format}\n" "$@" | awk '
+    cpac_shift_count=$(printf "${cpac_format}\n" "$@" | awk '
     BEGIN { count = 0; }
     {
       if (/^--$/) { print count; exit; }
@@ -226,40 +225,57 @@ do_install () {
     }
     END { print count; }
     ')
-    # echo sc=$shift_count
-    test ! -z "${package_spec_list}" && package_spec_count=$(expr ${package_spec_count} + 1)
-    for x in $(seq ${shift_count})
+    test ! -z "${cpac_package_spec_list}" && cpac_package_spec_count=$(expr ${cpac_package_spec_count} + 1)
+    for x in $(seq ${cpac_shift_count})
     do
         shift
     done
-    printf '%s\n' "${package_spec_list}" | while IFS= read -r spec
+    printf '%s\n' "${cpac_package_spec_list}" | while IFS= read -r cpac_spec
     do
-        # echo "-- [cpac] with spec=$spec" 1>&2
-        setup_current_package_variables "${spec}"
-        result=$?
-        show_current_package_variables 
-        test "$result" \
-            && retrive_source \
-            && configure_build_install "$@"
-        clear_source_dir
-        clear_binary_dir
+        cpac_setup_current_package_variables "${cpac_spec}"
+        cpac_result=$?
+        cpac_show_current_package_variables
+        test "$cpac_result" \
+            && cpac_retrive_source \
+            && cpac_configure_build_install "$@"
+        cpac_clear_source_dir
+        cpac_clear_binary_dir
     done
 }
 
-show_help_help() {
+cpac_repo () {
+    for cpac_spec in $@
+    do
+        cpac_setup_current_package_variables "${cpac_spec}" 2>/dev/null
+        echo "${cpac_repo}"
+    done
+}
+
+cpac_download () {
+    cpac_setup_current_package_variables "$1"
+    cpac_result=$?
+    test "$2" && cpac_source_dir="$2" || cpac_source_dir="${cpac_name}"
+    test "$cpac_result"  && cpac_retrive_source
+}
+
+cpac_help_help () {
     cat <<EOF
 usage:  cpac <operation> [...]
 operations:
     cpac {-h --help}
-    cpac {-S --sync} <package(s)> [-- <cmake_configure_option(s)>]
+    cpac {-S --sync}  <package(s)> [-- <cmake_configure_option(s)>]
+    cpac {--download} <package>    [path]
+    cpac {--repo}     <package(s)>
 
     use 'cpac {-h --help}' with an operation for available options
 EOF
 }
 
-show_help_sync() {
+cpac_help_sync () {
     cat <<EOF
 usage:  cpac {-S --sync} <package(s)> [-- <cmake_configure_option(s)>]
+description:
+  sync <package(s)>
 package:
   package can be specified with following syntax
     <package_name>[,<package_repo>][@[git_branch]]
@@ -276,22 +292,77 @@ example:
 EOF
 }
 
-show_help () {
+cpac_help_download () {
+    cat <<EOF
+usage:  cpac {--download} <package> [path]
+description:
+  download <package> to [path]
+EOF
+}
+
+cpac_help_repo () {
+    cat <<EOF
+usage:  cpac {--repo} <package(s)>
+description:
+  show list of repos specified by <package(s)>
+EOF
+}
+
+cpac_help () {
     case "$1" in
-    -S|--sync) show_help_sync ;;
-    -h|--help|*) show_help_help ;;
+    -S|--sync) cpac_help_sync ;;
+    --download) cpac_help_download ;;
+    --repo) cpac_help_repo ;;
+    -h|--help|*) cpac_help_help ;;
     esac
 }
 
-setup_common_variables
-load_package_metadata 
+cpac_load_default_init () {
+    cpac_load_default_package_metadata
+}
+
+cpac_load_init () {
+    if test -z "${cpac_load_init_disabled}"
+    then
+        for cpac_init_file in ~/.cpac.sh ~/.cpac.d/init.sh
+        do
+            if test -f "${cpac_init_file}"
+            then
+                source "${cpac_init_file}"
+                return 0
+            fi
+        done
+    fi
+    cpac_load_default_init
+}
+
+cpac_setup_common_variables () {
+    CPAC_COMMAND="$(readlink -f $0)"
+    cpac_this_dir="$(dirname ${CPAC_COMMAND})"
+    cpac_prefix_dir="$(dirname ${cpac_this_dir})"
+    CPAC_DEFAULT_PACKAGE_METADATA="${cpac_prefix_dir}/etc/cpac.d/packages.yml"
+    CPAC_BUILD_DIR="${cpac_prefix_dir}/var/cache/cpac"
+    CPAC_PREFIX_DIR="${cpac_prefix_dir}"
+    CPAC_DEFAULT_INIT="${cpac_prefix_dir}/etc/cpac.d/init.sh"
+}
+
+cpac_setup_common_variables
+cpac_load_init "$@"
 case "$1" in
-    -S|--sync)
-        shift
-        do_install "$@"
-        ;;
-    -h|--help|*)
-        shift
-        show_help "$@"
-        ;;
+-S|--sync)
+    shift
+    cpac_sync "$@"
+    ;;
+--download)
+    shift
+    cpac_download "$@"
+    ;;
+--repo)
+    shift
+    cpac_repo "$@"
+    ;;
+-h|--help|*)
+    shift
+    cpac_help "$@"
+    ;;
 esac
--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 ##
 ## Copyright (c) 2017 ChienYu Lin
 ##
-## Author: ChienYu Lin <cy20lin@gmail.com>
+## Author: ChienYu Lin <cy20lin@gmil.com>
 ## License: MIT
 ##
 
 this_dir="$(dirname $(readlink -f $0))"
 
-test -z "$1" && echo "Install failed." && exit 1
+test -z "$1" && echo "Install file." && exit 1
 test ! -d "$1/bin" && mkdir -p -- "$1/bin"
 cp -f -- "${this_dir}/bin/cpac.sh" "$1/bin/cpac"
 test ! -d "$1/etc/cpac.d" && mkdir -p -- "$1/etc/cpac.d"
